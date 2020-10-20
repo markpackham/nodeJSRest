9931ccbd5db4afeeaa7352488061ef563b846cc7
mark <markpackham1@gmail.com>
2020-10-20 17:41:46 +0100

    add Create, Update, Delete

--- controllers/productController.js
+++ controllers/productController.js
@@ -11,6 +11,92 @@ async function getProducts(req, res) {
   }
 }
 
+async function getProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(product));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function createProduct(req, res) {
+  try {
+    const body = await getPostData(req);
+
+    const { title, description, price } = JSON.parse(body);
+
+    const product = {
+      title,
+      description,
+      price,
+    };
+
+    const newProduct = await Product.create(product);
+
+    res.writeHead(201, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(newProduct));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      const body = await getPostData(req);
+
+      const { title, description, price } = JSON.parse(body);
+
+      const productData = {
+        title: title || product.title,
+        description: description || product.description,
+        price: price || product.price,
+      };
+
+      const updProduct = await Product.update(id, productData);
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(JSON.stringify(updProduct));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function deleteProduct(req, res, id) {
+  try {
+    const product = await Product.findById(id);
+
+    if (!product) {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: "Product Not Found" }));
+    } else {
+      await Product.remove(id);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: `Product ${id} removed` }));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 module.exports = {
   getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 };
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // Vanilla Node.js Rest API learned from https://www.youtube.com/watch?v=_1xa8Bsho6A
 const http = require("http");
-const { getProducts } = require("./controllers/productController");
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("./controllers/productController");
 
 const server = http.createServer((req, res) => {
   // res.statusCode = 200;
@@ -10,6 +16,28 @@ const server = http.createServer((req, res) => {
 
   if (req.url === "/api/products" && req.method === "GET") {
     getProducts(req, res);
+  } else if (
+    // use a regular expression to check for specific ids
+    // eg http://localhost:5000/api/products/1
+    req.url.match(/\/api\/products\/([0-9]+)/) &&
+    req.method === "GET"
+  ) {
+    const id = req.url.split("/")[3];
+    getProduct(req, res, id);
+  } else if (req.url === "/api/products" && req.method === "POST") {
+    createProduct(req, res);
+  } else if (
+    req.url.match(/\/api\/products\/([0-9]+)/) &&
+    req.method === "PUT"
+  ) {
+    const id = req.url.split("/")[3];
+    updateProduct(req, res, id);
+  } else if (
+    req.url.match(/\/api\/products\/([0-9]+)/) &&
+    req.method === "DELETE"
+  ) {
+    const id = req.url.split("/")[3];
+    deleteProduct(req, res, id);
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Route not found" }));
