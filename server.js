eebd1324a47c8fc36a7cb1c643838cf38ab1b81d
mark <markpackham1@gmail.com>
2020-10-20 10:45:11 +0100

    get all products, https://youtu.be/_1xa8Bsho6A?t=1212

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 // Vanilla Node.js Rest API learned from https://www.youtube.com/watch?v=_1xa8Bsho6A
 const http = require("http");
-const products = require("./data/products");
+const { getProducts } = require("./controllers/productController");
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h1>Hello World</h1>");
-  res.end();
+  // res.statusCode = 200;
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<h1>Hello World</h1>");
+  // res.end();
+
+  if (req.url === "/api/products" && req.method === "GET") {
+    getProducts(req, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
 });
 
 // check is there is an environment variable before using Port 5000
